a3e84d3eb06dbe48ce2d97a634c7027e8c02ed3b
Abner <alejandro-perry2011@hotmail.com>
2022-10-26 22:08:33 -0500

    PM json update, NATO FINISHED

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,32 +1,25 @@
+
 from tkinter import *
 from tkinter import messagebox
 import random
-import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
 def generate_password():
-
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
     nr_letters = random.randint(8, 10)
     nr_symbols = random.randint(2, 4)
     nr_numbers = random.randint(2, 4)
-
     password_letters = [random.choice(letters) for _ in range(nr_letters)]
     password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
     password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
-
     password_list = password_numbers + password_symbols + password_letters
-
     random.shuffle(password_list)
     password = "".join(password_list)
     password_entry.insert(0, password)
-    pyperclip.copy(password)
-
-
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
@@ -34,16 +27,46 @@ def save_data():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": username,
+            "password": password
+        }
+                }
     if website == "" or username == "" or password == "":
         messagebox.showinfo(message="Dejaste datos vacios")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"Data:\nusername: {username},\nPassword:{password}")
-        if is_ok:
-            with open("passwords.txt",  "a") as f:
-                f.write(f"{website} | {username} | {password}|\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+        try:
+            with open("data.json",  "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            with open("data.json", "w") as f:
+                json.dump(new_data, f, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as f:
+                json.dump(data, f, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+# ---------------------------- Search ---------------------------- #
 
+
+def search():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        messagebox.showinfo(message="Websites password not found, try to save some data")
+    else:
+        try:
+            email = data[website]["email"]
+            password = data[website]["password"]
+        except KeyError:
+            messagebox.showinfo(message="There's  no password for that website")
+        else:
+            messagebox.showinfo(message=f"Website: {website}\nEmail: {email}\nPassword: {password}")
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -54,27 +77,25 @@ canvas = Canvas(width=200, height=200, highlightthickness=0)
 lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
 canvas.grid(column=1, row=0)
-
-
 website_label = Label(text="Website: ")
 username_label = Label(text="Email/Username: ")
 password_label = Label(text="Password: ")
 generate_password_btn = Button(text="Generate Password", command=generate_password)
-website_entry = Entry(width=45)
+website_entry = Entry(width=26)
 username_entry = Entry(width=45)
 password_entry = Entry(width=26)
 email = StringVar()
 add_btn = Button(text="Add", width=42, command=save_data)
-
+search_btn = Button(text="Search", width=16, command=search)
 username_entry.insert(0, "alejandro-perry2011@hotmail.com")
 website_label.grid(column=0, row=1)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1)
 username_label.grid(column=0, row=2)
 username_entry.grid(column=1, row=2, columnspan=2)
 password_label.grid(column=0, row=3)
 password_entry.grid(column=1, row=3)
 generate_password_btn.grid(column=2, row=3)
 add_btn.grid(column=1, row=4, columnspan=2)
-
+search_btn.grid(column=2, row=1)
 
 window.mainloop()
